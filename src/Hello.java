d03d93bf2316220a24efb3f19de9dcbc0f9abd1e
houyu611 <houyu@houyudeMacBook-Pro.local>
2014-07-17 03:02:03 +0800

    this is the one

--- src/Hello.java
+++ src/Hello.java
@@ -3,8 +3,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.print("Hello World!!");
-			System.out.print("the first git");
+			System.out.println("Hello World!!");
+			System.out.println("the first git");
+			System.out.print("this is the one");
 	}
 
 }
