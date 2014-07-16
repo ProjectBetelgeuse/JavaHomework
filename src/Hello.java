419ce65d096d754722a705f6834e0a77b7f67b68
houyu611 <houyu@houyudeMacBook-Pro.local>
2014-07-17 03:06:01 +0800

    Revert "this is the one"
    This reverts commit d03d93bf2316220a24efb3f19de9dcbc0f9abd1e.

--- src/Hello.java
+++ src/Hello.java
@@ -3,9 +3,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hello World!!");
-			System.out.println("the first git");
-			System.out.print("this is the one");
+			System.out.print("Hello World!!");
+			System.out.print("the first git");
 	}
 
 }
