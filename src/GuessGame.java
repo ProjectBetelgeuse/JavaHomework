8a28805e09820b3c29af7192d9ef65903b7d28ab
houyu611 <houyu@houyudeMacBook-Pro.local>
2014-07-18 09:54:12 +0800

    intro of the game

---
+++ src/GuessGame.java
@@ -0,0 +1,15 @@
+
+public class GuessGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    	
+	}
+    public static void intro(){
+    	System.out.println("This program allows you to play a guessing game.");
+    	System.out.println("I will think of a number between 1 and");
+    	System.out.print("100 and will allow you to guess until");
+    	
+    	
+    }
+}
