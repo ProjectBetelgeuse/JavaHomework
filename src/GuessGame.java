fceeb3c8f6069b702c57714c54730ea4173d370a
houyu611 <houyu@192.168.1.103>
2014-07-21 17:44:19 +0800

    More intro

--- src/GuessGame.java
+++ src/GuessGame.java
@@ -3,13 +3,17 @@ public class GuessGame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    	
+    	intro();
 	}
+	/*
+	 * the intro of the game
+	 */
     public static void intro(){
     	System.out.println("This program allows you to play a guessing game.");
     	System.out.println("I will think of a number between 1 and");
-    	System.out.print("100 and will allow you to guess until");
-    	
-    	
+    	System.out.println("100 and will allow you to guess until");
+    	System.out.println("you get it. For each guess, I will tell you");
+    	System.out.println("whether the right answer is higher or lower");
+    	System.out.print("than your guess.");
     }
 }
