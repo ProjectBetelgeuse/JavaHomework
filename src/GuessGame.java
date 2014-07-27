7c56173cceb2a375f41c86ee521c3f759c246153
houyu611 <houyu@bogon>
2014-07-27 14:42:12 +0800

    the game still lack of the ability to count

--- src/GuessGame.java
+++ src/GuessGame.java
@@ -1,9 +1,10 @@
-
+import java.util.*;
 public class GuessGame {
-
+public static int MaxNum = 100;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     	intro();
+    	game();
 	}
 	/*
 	 * the intro of the game
@@ -14,10 +15,24 @@ public class GuessGame {
     	System.out.println("100 and will allow you to guess until");
     	System.out.println("you get it. For each guess, I will tell you");
     	System.out.println("whether the right answer is higher or lower");
-    	System.out.print("than your guess.");
+    	System.out.println("than your guess.");
+    	System.out.println();
     }
 
 	public static void game(){
-		
+		Scanner in=new Scanner(System.in);
+		int RNum=(int)(Math.random()*MaxNum);
+		System.out.println("I'm thinking of a number between 1 and "+MaxNum+"...");
+		for (int a = 0;a<MaxNum/2;a++){
+			System.out.print("your guess?");
+			int UserNum=in.nextInt();
+			if (UserNum<RNum){
+				System.out.println("It's higher");
+			}if (UserNum>RNum){
+				System.out.println("It's lower");
+			}else{
+				System.out.println("you got it right in "+"guesses");
+			}
+		}
 	}
 }
