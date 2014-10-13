2083fdf81c7307c0651c24768bab0d387de8c700
houyu611 <houyu@D-10-154-215-22.dhcp4.washington.edu>
2014-10-13 15:26:15 -0700

    new homework

--- src/Homework/AsciiArt.java
+++ src/Homework/AsciiArt.java
@@ -1,59 +1,60 @@
-/*Yu Hou
- *10/7/2014
- *CSE 142A
- * TA:Christopher R. Gores
- * Assignment #2
- * 
- * This program drawing a heart in "*".
- */
-package Homework;
+/* Yu Hou
+* 10/7/2014
+* CSE 142A
+* TA:Christopher R. Gores
+* Assignment #2
+*
+* This program drawing a heart in "*".
+*/
+
 public class AsciiArt {
-	
-	public static void main(String[] args) {
-		upperPart();
-		line();
-		line();
-		downPart();
-	}
-	
-	public static void upperPart(){
-		for(int a=1;a<7;a++){
-			for(int b=7-a;b>0;b--){
-				System.out.print(" ");
-			}
-			for(int c=0;c<1+2*a;c++){
-				System.out.print("*");
-			}
-			for(int d=0;d<12-2*a;d++){
-				System.out.print(" ");
-			}
-			for(int e=0;e<1+2*a;e++){
-				System.out.print("*");
-			}
-			for(int f=7-a;f>0;f--){
-				System.out.print(" ");
-			}
-			System.out.println();
-		}
-	}
-	
-	public static void downPart(){
-		for(int g=0;g<14;g++){
-			for(int h=0;h<g;h++){
-				System.out.print(" ");
-			}
-			for(int i=0;i<28-2*g;i++){
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-	
-	}
-	
-	public static void line(){
-		for(int x=0;x<28;x++){
-			System.out.print("*");
-		}
-		System.out.println();
-	}
+   
+   public static void main(String[] args) {
+      upperPart();
+      line();
+      line();
+      downPart();
+   }
+   
+   public static void upperPart(){ //this part drawing the upper round of a heart.
+      for(int a=1;a<7;a++){
+         for(int b=7-a;b>0;b--){
+            System.out.print(" ");
+         }
+         for(int c=0;c<1+2*a;c++){
+            System.out.print("*");
+         }
+         for(int d=0;d<12-2*a;d++){
+            System.out.print(" ");
+         }
+         for(int e=0;e<1+2*a;e++){
+            System.out.print("*");
+         }
+         for(int f=7-a;f>0;f--){
+            System.out.print(" ");
+         }
+         System.out.println();
+      }
+   }
+   
+   public static void downPart(){ //this part drawing the down part of this heart
+      for(int g=0;g<14;g++){
+         for(int h=0;h<g;h++){
+            System.out.print(" ");
+         }
+         for(int i=0;i<28-2*g;i++){
+            System.out.print("*");
+         }
+         System.out.println();
+      }
+      
+   }
+   
+   public static void line(){ //this part is the line between the upper and the down part in order to make this graphic look more like a heart.
+      for(int x=0;x<28;x++){
+         System.out.print("*");
+      }
+      System.out.println();
+   }
 }
+
--- src/Homework/DrawRocket.java
+++ src/Homework/DrawRocket.java
@@ -1,96 +1,96 @@
 /* Yu Hou
- * 10/2/2014
- * CSE 142A
- * TA: Christopher R. Gores
- * Assignment #2
- * 
- * This program use for loop to print a Rocket.
- * You can change the Rocket by change the constant "size".
- */
+* 10/2/2014
+* CSE 142A
+* TA: Christopher R. Gores
+* Assignment #2
+*
+* This program use for loop to print a Rocket.
+* You can change the Rocket by change the constant "size".
+*/
 
-package Homework;
 public class DrawRocket {
-	
-public static final int size=5;//You can change this number in order to change the whole size of this Rocket.
-
-	public static void main(String[] args) {
-		triangle();
-		line();
-		frontPart();
-		backPart();
-		line();
-		backPart();
-		frontPart();
-		line();
-		triangle();
-	 }
-	
-	public static void triangle(){   //Print out the top and the end of the Rocket
-		for(int x=1;x<=2*size-1;x++){
-			for(int y=2*size-1-x;y>=0;y--){
-				System.out.print(" ");
-			}
-			for(int z=0;z<x;z++){
-				System.out.print("/");
-			}
-			System.out.print("**");
-			for(int c=0;c<x;c++){
-				System.out.print("\\");
-			}
-			System.out.println();
-		}
-
-	}
-	
-	public static void line(){   //Print out the line between the body and head of the Rocket
-		System.out.print("+");
-		for(int b=1;b<=(size*2);b++){
-			System.out.print("=*");
-		}
-		System.out.println("+");
-	}
-	
-	public static void frontPart(){    //Front part of the body of the Rocket
-		for(int x=1;x<=size;x++){
-			System.out.print("|");
-			for(int y=size-x;y>0;y--){
-				System.out.print(".");
-			}
-			for(int z=0;z<x;z++){
-				System.out.print("/\\");
-			}
-			for(int h=2*(size-x);h>0;h--){
-				System.out.print(".");
-			}
-			for(int a=0;a<x;a++){
-				System.out.print("/\\");
-			}
-			for(int y=size-x;y>0;y--){
-				System.out.print(".");
-			}
-			System.out.println("|");
-		}
-	}
-	
-	public static void backPart(){    //Back part of the body of the Rocket
-		for(int x=size;x>0;x--){
-			System.out.print("|");
-			for(int y=size-x;y>0;y--){
-				System.out.print(".");
-			}
-			for(int z=0;z<x;z++){
-				System.out.print("\\/");
-			}
-			for(int h=2*(size-x);h>0;h--){
-				System.out.print(".");
-			}
-			for(int z=0;z<x;z++){
-				System.out.print("\\/");
-			}
-			for(int y=size-x;y>0;y--){
-				System.out.print(".");
-			}
-			System.out.println("|");
-		}
-	}
+   
+   public static final int size=5;//You can change this number in order to change the whole size of this Rocket.
+   
+   public static void main(String[] args) {
+      triangle();
+      line();
+      frontPart();
+      backPart();
+      line();
+      backPart();
+      frontPart();
+      line();
+      triangle();
+   }
+   
+   public static void triangle(){   //Print out the top and the end of the Rocket
+      for(int x=1;x<=2*size-1;x++){
+         for(int y=2*size-1-x;y>=0;y--){
+            System.out.print(" ");
+         }
+         for(int z=0;z<x;z++){
+            System.out.print("/");
+         }
+         System.out.print("**");
+         for(int c=0;c<x;c++){
+            System.out.print("\\");
+         }
+         System.out.println();
+      }
+      
+   }
+   
+   public static void line(){   //Print out the line between the body and head of the Rocket
+      System.out.print("+");
+      for(int b=1;b<=(size*2);b++){
+         System.out.print("=*");
+      }
+      System.out.println("+");
+   }
+   
+   public static void frontPart(){    //Front part of the body of the Rocket
+      for(int x=1;x<=size;x++){
+         System.out.print("|");
+         for(int y=size-x;y>0;y--){
+            System.out.print(".");
+         }
+         for(int z=0;z<x;z++){
+            System.out.print("/\\");
+         }
+         for(int h=2*(size-x);h>0;h--){
+            System.out.print(".");
+         }
+         for(int a=0;a<x;a++){
+            System.out.print("/\\");
+         }
+         for(int y=size-x;y>0;y--){
+            System.out.print(".");
+         }
+         System.out.println("|");
+      }
+   }
+   
+   public static void backPart(){    //Back part of the body of the Rocket
+      for(int x=size;x>0;x--){
+         System.out.print("|");
+         for(int y=size-x;y>0;y--){
+            System.out.print(".");
+         }
+         for(int z=0;z<x;z++){
+            System.out.print("\\/");
+         }
+         for(int h=2*(size-x);h>0;h--){
+            System.out.print(".");
+         }
+         for(int z=0;z<x;z++){
+            System.out.print("\\/");
+         }
+         for(int y=size-x;y>0;y--){
+            System.out.print(".");
+         }
+         System.out.println("|");
+      }
+   }
 }
+
