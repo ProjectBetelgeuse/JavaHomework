b7b4e2ce720dc6850a6a0f4408123fa43655c27c
houyu611 <houyu@D-10-154-197-177.dhcp4.washington.edu>
2014-11-12 16:36:44 -0800

    Working version

--- src/Hello.java
+++ src/Hello.java
@@ -2,23 +2,7 @@ import java.util.*;
 public class Hello {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner("1 2 3 4");
-		processData(input);
-	}
-	public static void processData(Scanner input){
-		int count = 1;
-		int predigits = 0;
-		int sum = 0;
-		double average = 0;
-		while (input.hasNextInt()){
-			int digit = input.nextInt();
-			sum = digit+predigits;
-			predigits = sum;
-			System.out.println("Sum of "+count+" = "+sum);
-			count++;
-		}
-		average = (double)sum/(count-1);
-		System.out.print("Average = "+average);
+		System.out.print((int)Math.round(733*0.5)+24);
 	}
 }
 
--- src/Homework/Names.java
+++ src/Homework/Names.java
@@ -5,6 +5,10 @@ import java.awt.*;
 import java.io.*;
 
 public class Names {
+	
+	public static final int decades = 14;
+	public static final int years = 1880;
+	public static final int width = 70;
 
 	public static void main(String[] args) 
 			throws FileNotFoundException {
@@ -15,9 +19,12 @@ public class Names {
 		String name = console.next().toLowerCase();
 		System.out.print("gender (M or F)? ");
 		String gender = console.next().toLowerCase();
-		find(input,name,gender);
-		
-		
+		String line = find(input, name, gender);
+		if(line.length()==0){
+			System.out.println("name/gender combination not found");
+		}else{
+			lineScan(line);
+		}
 	}
 	public static void intro(){
 		System.out.println("This program allows you to search through the");
@@ -27,20 +34,62 @@ public class Names {
 		System.out.println();
 	}
 	
-	public static void find(Scanner input, String name, String gender){
+	public static String find(Scanner input, String name, String gender){
 		while(input.hasNextLine()){
 			String line = input.nextLine();
-			Scanner lineScan = new Scanner (line);
-			boolean findName = lineScan.next().toLowerCase().equals(name);
-			boolean findGender = lineScan.next().toLowerCase().equals(gender);
-			if (findName&&findGender){
-				System.out.print("the");
+			Scanner lineBase = new Scanner(line);
+			boolean findName = lineBase.next().toLowerCase().equals(name);
+			boolean findGender = lineBase.next().toLowerCase().equals(gender);
+			if(findGender&&findName){
+				return line;
 			}
 		}
+		return "";	
 	}
-	public static void paint(){
-		DrawingPanel p = new DrawingPanel(500,500);
+	
+	public static void lineScan(String line){
+		DrawingPanel p = new DrawingPanel(decades*width,550);
 		Graphics g = p.getGraphics();
+		grid(p,g);
+		Scanner lineBase = new Scanner(line);
+		String name = lineBase.next();
+		String gender = lineBase.next();
+		int num = 0;
+		int count = 0;
+		int preNum = lineBase.nextInt();
+		int pre = 0;
+		g.setColor(Color.RED);
+		if(preNum==0){
+			g.drawString(name+" "+gender+" "+preNum, count*width, 525);
+			pre = 525;
+		}else{
+			g.drawString(name+" "+gender+" "+preNum, count*width, (int)(Math.round(preNum*0.5)+24));
+			pre = (int)(Math.round(preNum*0.5)+24);
+		}
+		while(lineBase.hasNextInt()){
+			num = lineBase.nextInt();
+			if(num==0){
+				g.drawString(name+" "+gender+" "+num, (count+1)*width, 525);
+				g.drawLine(count*width, pre, (count+1)*width, 525);
+				pre = 525;
+			}else{
+				g.drawString(name+" "+gender+" "+num, (count+1)*width, (int) (Math.round(num*0.5)+24));
+				g.drawLine(count*width, pre, (count+1)*width, (int)(Math.round(num*0.5)+24));
+				pre = (int)(Math.round(num*0.5)+24);
+			}
+			preNum=num;
+			count++;
+		}
+	}
+	
+	public static void grid(DrawingPanel p, Graphics g){
+		g.drawLine(0, 25, decades*width, 25);
+		g.drawLine(0, 525, decades*width, 525);
+		for(int i = 0;i<decades;i++){
+			g.drawLine(i*width, 0, i*width, 550);
+			g.drawString(""+(years+(i*10)), i*width, 550);
+		}
+		
 	}
 	
 }
